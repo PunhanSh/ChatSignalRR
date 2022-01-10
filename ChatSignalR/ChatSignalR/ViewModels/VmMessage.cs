3feb2a099629f4f4ce1543c9c6c40ec463839e97
PunhanSh <punhanash@code.edu.az>
2022-01-10 13:58:33 +0400

    Added Task

---
+++ ChatSignalR/ChatSignalR/ViewModels/VmMessage.cs
@@ -0,0 +1,15 @@
+﻿using ChatSignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.ViewModels
+{
+    public class VmMessage
+    {
+        public List<Message> Messages { get; set; }
+        public string SenderId { get; set; }
+        public CustomUser User { get; set; }
+    }
+}
